81282e2e21893d102184daf043902a41f53c2ed2
Francisco Lazarte Salazar <202401362@est.umss.edu>
2026-06-20 19:44:05 -0400

    chore: inicialización del proyecto Spring Boot

---
+++ study-match/src/main/java/com/flazarte/study_match/StudyMatchApplication.java
@@ -0,0 +1,13 @@
+package com.flazarte.study_match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMatchApplication.class, args);
+	}
+
+}
---
+++ study-match/src/test/java/com/flazarte/study_match/StudyMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flazarte.study_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
