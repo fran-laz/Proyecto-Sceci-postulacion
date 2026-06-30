4d945659ece3b6f32cf96a4d31ba3974e7f03cb0
Francisco Lazarte Salazar <202401362@est.umss.edu>
2026-06-29 22:28:26 -0400

    feat: integrate Spring Security and JWT, setup JwtUtil and SecurityConfig

---
+++ study-match/src/main/java/com/flazarte/study_match/config/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.flazarte.study_match.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private static final long EXPIRATION_TIME = 86400000;
+
+    public String generarToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public String extraerEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validarToken(String token, String email) {
+        return (email.equals(extraerEmail(token)) && !isTokenExpired(token));
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+}
