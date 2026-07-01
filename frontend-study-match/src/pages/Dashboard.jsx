84d60071e945dca0acfd60b949c16a9a81744ca8
Francisco Lazarte Salazar <202401362@est.umss.edu>
2026-07-01 15:27:49 -0400

    feat: install react-router-dom, set up routing navigation, and create Login/Dashboard pages

---
+++ frontend-study-match/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h2>Panel Principal (Dashboard)</h2>
+      <p>lista de materias y proyectos de estudio</p>
+    </div>
+  );
+}
---
+++ frontend-study-match/src/pages/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div>
+      <h2>Pantalla de Login</h2>
+      <p>formulario para iniciar sesión.</p>
+    </div>
+  );
+}
