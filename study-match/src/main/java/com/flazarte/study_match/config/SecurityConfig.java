3dd435cbe73fdc8519219029de9deb7db1087112
Francisco Lazarte Salazar <202401362@est.umss.edu>
2026-07-05 15:57:04 -0400

    fix: add vercel URL to Cors

--- study-match/src/main/java/com/flazarte/study_match/config/SecurityConfig.java
+++ study-match/src/main/java/com/flazarte/study_match/config/SecurityConfig.java
@@ -48,9 +48,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:*",
+                "https://studymatch-ecru-six.vercel.app"
+        ));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
